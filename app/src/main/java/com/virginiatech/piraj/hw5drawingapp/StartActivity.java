3cde0873d25c0c7e0de6bdb32a024d4525173c14
Rikkokiri <pilvi.rajala@gmail.com>
2016-11-23 16:23:18 -0500

    Added external storage permissions

--- app/src/main/java/com/virginiatech/piraj/hw5drawingapp/StartActivity.java
+++ app/src/main/java/com/virginiatech/piraj/hw5drawingapp/StartActivity.java
@@ -2,12 +2,27 @@ package com.virginiatech.piraj.hw5drawingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button takePictureButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        //Button that is used to take a picture
+        takePictureButton = (Button) findViewById(R.id.takePicture);
+
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
