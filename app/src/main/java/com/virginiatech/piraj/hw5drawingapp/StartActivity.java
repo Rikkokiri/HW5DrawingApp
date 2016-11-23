cf9afc2ae52e35f573e7159e7ac515bb946ff228
Rikkokiri <pilvi.rajala@gmail.com>
2016-11-23 16:37:40 -0500

    If permissions are granted, enable picture taking button

--- app/src/main/java/com/virginiatech/piraj/hw5drawingapp/StartActivity.java
+++ app/src/main/java/com/virginiatech/piraj/hw5drawingapp/StartActivity.java
@@ -1,5 +1,9 @@
 package com.virginiatech.piraj.hw5drawingapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,12 +21,28 @@ public class StartActivity extends AppCompatActivity {
         //Button that is used to take a picture
         takePictureButton = (Button) findViewById(R.id.takePicture);
 
-        takePictureButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        //Check permissions, disable picture taking button if we don't have camera permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            takePictureButton.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
 
+    }
+
+    /**
+     * If user grants us permissions to use the camera, enable the picture taking button
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                takePictureButton.setEnabled(true);
             }
-        });
-
+        }
     }
 }
