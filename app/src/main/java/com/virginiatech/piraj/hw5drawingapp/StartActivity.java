27e5dda8da7b754705aa0689d97e2ad5a5fa96c0
Rikkokiri <pilvi.rajala@gmail.com>
2016-11-23 16:07:23 -0500

    Renamed MainActivity to StartActivity

--- app/src/main/java/com/virginiatech/piraj/hw5drawingapp/StartActivity.java
+++ app/src/main/java/com/virginiatech/piraj/hw5drawingapp/StartActivity.java
@@ -3,11 +3,11 @@ package com.virginiatech.piraj.hw5drawingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
     }
 }
